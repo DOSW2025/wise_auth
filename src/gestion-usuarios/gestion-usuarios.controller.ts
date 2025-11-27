b44c961d805b637e020a8cc0e78583b604346717
Daniel Eduardo Useche <du.important@gmail.com>
2025-11-27 10:39:26 -0500

    Develop (#31)
    * Feat/auth/user management (#29)
    
    * fix: update Prisma schema and user service to use new role and status structure
    
    * fix: update AuthController to use gatewayUrl instead of frontendUrl for redirects
    
    * fix: update JWT_SECRET in .env.template for consistency
    
    * fix: PrismaClient
    
    * fix:package.json
    
    * fix:package.json
    
    * fix:package
    
    * fix: update start scripts in package.json to remove prisma migrate deploy
    
    * fix: add environment variables for DATABASE_URL and DIRECT_URL in CI workflow
    
    * fix: move prisma migrate deploy to prestart script in package.json
    
    * fix: update npm commands and refine artifact upload in CI workflow
    
    * fix: update prisma commands to use npx for consistency in package.json scripts
    
    * fix: update Prisma commands to use direct node execution for consistency in package.json scripts
    
    * fix: update start scripts to generate Prisma Client before migrations
    
    * fix: standardize log messages in Prisma Client generation scripts
    
    * fix: enhance environment variable debugging and validation logging
    
    * feat: implement FilterUsersDto and update user retrieval with filtering options
    
    ---------
    
    Co-authored-by: Nella1414 <marianella.polo-p@mail.escuelaing.edu.co>
    
    * Fix/logica usuarios suprimida (#30)
    
    * refactor: remove user management module and related services
    
    * fix(schema): add tutorias
    
    ---------
    
    Co-authored-by: Nella1414 <marianella.polo-p@mail.escuelaing.edu.co>

--- src/gestion-usuarios/gestion-usuarios.controller.ts
+++ src/gestion-usuarios/gestion-usuarios.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get, Patch, Delete, Param, Body, Query, UseGuards } from '@nestjs/common';
 import { GestionUsuariosService } from './gestion-usuarios.service';
-import { PaginationDto } from '../common/dto/pagination.dto';
-import { ChangeRoleDto, ChangeStatusDto, UpdatePersonalInfoDto } from './dto';
+import { ChangeRoleDto, ChangeStatusDto, UpdatePersonalInfoDto, FilterUsersDto } from './dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { Role } from '../auth/enums/role.enum';
 import { GetUser } from '../auth/decorators/get-user.decorator';
+import { Public } from 'src/auth';
 
 @Controller('gestion-usuarios')
 @UseGuards(JwtAuthGuard, RolesGuard)
@@ -15,8 +15,8 @@ export class GestionUsuariosController {
 
   @Get()
   @Roles(Role.ADMIN)
-  findAll(@Query() paginationDto: PaginationDto) {
-    return this.gestionUsuariosService.findAllPaginated(paginationDto);
+  findAll(@Query() filterUsersDto: FilterUsersDto) {
+    return this.gestionUsuariosService.findAllWithFilters(filterUsersDto);
   }
 
   @Patch(':id/rol')
